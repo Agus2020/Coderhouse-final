cdb5bf00942b1cbf3235cb013a811dac2330b40e
Agus2020 <aagustin067@gmail.com>
2022-11-27 18:55:40 -0300

    Se modifico la maquetacion del item

--- src/Item.js
+++ src/Item.js
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom"
 
 const Item = ({ title, image, price, id }) => {
     return (
-        <article className="product__card card">
-            <h1 className="card__title">{title} </h1>
+        <div className="card card-r" >
             <h2>${price}</h2>
-            <img src={image} alt={title} className="card__img" />
-            <button className="card__button">
+            <img src={image} alt={title} className="card-img-top card-img-r" />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <button className="card__button">
                 <Link to={"/item/" + id} >
                     Ver Detalle
                 </Link>
             </button>
-        </article>
+            </div>
+            
+           
+        </div>
+        
     )
 }
 export default Item
