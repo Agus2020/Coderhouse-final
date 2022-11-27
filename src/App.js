949ce2a082f46f9475cbccba898016fb35850653
Agus2020 <aagustin067@gmail.com>
2022-11-27 18:54:53 -0300

    Se eliminaron librerias que no se usaran

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Header from "./Header"
 import Main from "./Main"
-import Footer from "./Footer"
 import { BrowserRouter } from "react-router-dom"
-import { ToastContainer } from "react-toastify"
 import CustomProvider from "./CustomProvider"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,8 +10,6 @@ function App() {
             <BrowserRouter>
                 <Header />
                 <Main />
-                <Footer />
-                <ToastContainer />
             </BrowserRouter>
         </CustomProvider>
     )
