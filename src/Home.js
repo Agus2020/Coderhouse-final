0455e73e7c9bfd08efec2e1c1d371f1a9ef5652c
Agus2020 <aagustin067@gmail.com>
2022-12-12 20:52:17 -0300

    Se agrego un Banner al Home

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,29 @@
+
+
+
 const Home = () => {
-    
     return (
-        <div>
-            
-        </div>
+<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="./banner.jpg " className="d-block w-100 r-banner" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="./banner2.gif " className="d-block w-100 r-banner" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="./banner3.jpg" className="d-block w-100 r-banner" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
     )
 }
 
