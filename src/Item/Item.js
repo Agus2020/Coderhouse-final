df9ba07113d8ceb70dc8bbbbd652f4e698531cd6
Agus2020 <aagustin067@gmail.com>
2022-12-12 20:52:50 -0300

    Se agregaron mas estilos y se añadio un contador de stock

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
 
 
-const Item = ({ title, images, price, id }) => {
+const Item = ({ title, images, price, id,stock }) => {
     return (
         <div className="card card-r" >
             <h2>US${price}</h2>
             <img src={images} alt={title} className="card-img-top card-img-r" />
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
+                <h5>Cantidad:{stock}</h5>
                 <button className="btn btn-primary">
+                    
                 <Link to={"/item/" + id} className="text-light">
                     Ver Detalle
                 </Link>
